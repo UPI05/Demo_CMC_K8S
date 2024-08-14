4133bef0c1b0edcf80a9cf02d1271934cf5473fc
Hieu Vo <votruongtrunghieu@gmail.com>
2024-08-14 11:20:35 +0700

    Update CI workflow

--- frontend/build/env.js
+++ frontend/build/env.js
@@ -4,7 +4,7 @@ window.env = {
   "APPDATA": "C:\\Users\\votru\\AppData\\Roaming",
   "ChocolateyInstall": "C:\\ProgramData\\chocolatey",
   "ChocolateyLastPathUpdate": "133679091809243261",
-  "CHROME_CRASHPAD_PIPE_NAME": "\\\\.\\pipe\\crashpad_13452_SJEVETQGYAJDWWZK",
+  "CHROME_CRASHPAD_PIPE_NAME": "\\\\.\\pipe\\crashpad_14632_HKXSPSZRBEGIQTYF",
   "COLOR": "1",
   "COLORTERM": "truecolor",
   "CommonProgramFiles": "C:\\Program Files\\Common Files",
