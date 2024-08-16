0f4000a6d78cf6922560b17707824dfd0762d548
Hieu Vo <votruongtrunghieu@gmail.com>
2024-08-16 12:00:01 +0700

    update

--- express_server.js
+++ express_server.js
@@ -48,7 +48,7 @@ const Account = mongoose.model('users', accountSchema);
 
 
 app.get('/hello', (req, res) => {
-  res.send('CMC K8s demo!!! By HieuVo.');
+  res.send('CMC K8s demo!!! By HieuVo. Test CICD.');
 });
 
 app.post('/login', (req, res) => {
